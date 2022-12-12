e13b56b37930946abef9d963858f6d76f274c15a
Matthew Compton <matthewcompton49@gmail.com>
2022-12-12 20:46:35 +0000

    enh: add method

--- src/components/Recipe/Method/Method.tsx
+++ src/components/Recipe/Method/Method.tsx
@@ -1,9 +1,15 @@
 import {
+  Avatar,
   Card,
   CardContent,
   CardHeader,
   Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { Step } from "./types";
 
@@ -12,16 +18,34 @@ type Props = {
 };
 
 export default function Method({ steps }: Props) {
+  const theme = useTheme();
+
   return (
     <Card>
       <CardHeader title="Method" />
       <Divider />
-      <CardContent>
-        {steps.map((step) => (
-          <Typography component="p" variant="body1">
-            {step.text}
-          </Typography>
-        ))}
+      <CardContent sx={{ pt: 1 }}>
+        <List disablePadding>
+          {steps
+            .sort((a, b) => (a.step > b.step ? 1 : -1))
+            .map((step) => (
+              <>
+                <ListItem alignItems="flex-start" disableGutters sx={{ mt: 2 }}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ bgcolor: theme.colors.secondary.dark }}>
+                      <Typography>{step.step}</Typography>
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText>
+                    <Typography component="p" variant="body1">
+                      {step.text}
+                    </Typography>
+                  </ListItemText>
+                </ListItem>
+                <Divider />
+              </>
+            ))}
+        </List>
       </CardContent>
     </Card>
   );
